3a9730a74baf04d46306e6b16f0f10471435f892
AmgadSa <AmgadSa@outlook.com>
2022-11-29 23:34:59 +0300

    Add script to populate the page and card icons

---
+++ script.js
@@ -0,0 +1,72 @@
+const vowel = 'aeeioou';
+const con = 'BCCCDDDFGHJKLMMMNNNPQRSSSTTVXZWY'.toLocaleLowerCase();
+
+
+const projects = document.querySelector('.projects');
+const announcements = document.querySelector('.announcements');
+const trending = document.querySelector('.trending');
+
+
+const random = (x=10)=> Math.floor(Math.random()*(x+1));
+
+const create = (x)=> document.createElement(x);
+
+const getSyllable = ()=>{
+    return con[random(con.length-1)]+vowel[random(vowel.length-1)];
+};
+const getWord = (x=random(7))=>{
+    let word = (x<=3) ? getSyllable()+con[random(con.length-1)]:
+    [...Array(Math.floor(x/2))].map( item => 
+        (random(10)<=5) ? getSyllable() :
+        getSyllable()+con[random(con.length-1)]
+    ).join('');
+    return word;
+};
+const getParagraph = (x) => [...Array(x)].map(item => getWord()).join(' ');
+
+const getContent = ()=>{
+    let content = create('div');
+    let title = create('h1');
+    let par = create('p');
+
+    title.className = 'section-headings';
+
+    title.textContent = getParagraph(2+random(2));
+    par.innerText = getParagraph(25+random(5));
+
+    [title,par].forEach(content.appendChild,content);
+
+    return content;
+};
+const getCard = ()=>{
+    let card = create('div');
+    card.appendChild(getContent());
+    ['favor','watch','fork'].map(x=>{
+        let y = create('img');
+        y.src = 'images/'+x+'.svg';
+        y.alt = 'card icon';
+        return y;
+    }).forEach(card.appendChild,card);
+    return card;
+};
+const getUser = ()=>{
+    let user = create('div');
+    let icon = create('span');
+    let name = create('span');
+    let proj = create('span');
+
+    icon.className = 'user-icon';
+    icon.textContent = (con+vowel)[random((con+vowel).length-1)];
+    name.textContent = '@'+getWord();
+    proj.textContent = getParagraph(3);
+
+    user.className = 'user-detail';
+    [icon,name,proj].forEach(user.appendChild,user);
+    return user;
+}
+const populate = (x,y,z)=>{
+    [...Array(x)].map(item => getCard()).forEach(projects.appendChild,projects);
+    [...Array(y)].map(item => getContent()).forEach(announcements.appendChild,announcements);
+    [...Array(x)].map(item => getUser()).forEach(trending.appendChild,trending);
+};
+populate(6,3,4);
